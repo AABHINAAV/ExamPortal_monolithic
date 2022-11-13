f6b1ec18989052ee7e8b007de50d79f9852ed583
aabhinaav <abhinavgargchand@gmail.com>
2022-11-13 19:12:52 +0530

    authority class added with implementation
    UserDetails interface implemented in User model

--- examserver/src/main/java/com/exam/models/User.java
+++ examserver/src/main/java/com/exam/models/User.java
@@ -1,5 +1,6 @@
 package com.exam.models;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,136 +14,164 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "users")
-public class User {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	private String username;
-	private String password;
-	private String firstName;
-	private String lastName;
-	private String email;
-	private String phone;
-	private boolean enabled = true;
-	private String profile;
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private boolean enabled = true;
+    private String profile;
 
-	// single user can have many user roles
-	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "user")
-	@JsonIgnore
-	private Set<UserRole> userRoles = new HashSet<>();
+    // single user can have many user roles
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "user")
+    @JsonIgnore
+    private Set<UserRole> userRoles = new HashSet<>();
 
-	// default constructor
-	public User() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+    // default constructor
+    public User() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-//	parameterised constructor
-	public User(Long id, String username, String password, String firstName, String lastName, String email,
-			String phone, boolean enabled, String profile, Set<UserRole> userRoles) {
-		super();
-		this.id = id;
-		this.username = username;
-		this.password = password;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.phone = phone;
-		this.enabled = enabled;
-		this.profile = profile;
-		this.userRoles = userRoles;
-	}
+    //	parameterised constructor
+    public User(Long id, String username, String password, String firstName, String lastName, String email,
+                String phone, boolean enabled, String profile, Set<UserRole> userRoles) {
+        super();
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.enabled = enabled;
+        this.profile = profile;
+        this.userRoles = userRoles;
+    }
 
-//	getter setters
-	public Long getId() {
-		return id;
-	}
+    //	getter setters
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<Authority> authorities = new HashSet<>();
 
-	public String getLastName() {
-		return lastName;
-	}
+        this.userRoles.forEach(userRole -> {
+            authorities.add(new Authority(userRole.getRole().getRoleName()));
+        });
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+        return null;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public String getPhone() {
-		return phone;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public boolean isEnabled() {
-		return enabled;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public void setEnabled(boolean enabled) {
-		this.enabled = enabled;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public String getProfile() {
-		return profile;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setProfile(String profile) {
-		this.profile = profile;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public Set<UserRole> getUserRoles() {
-		return userRoles;
-	}
+    public String getPhone() {
+        return phone;
+    }
 
-	public void setUserRoles(Set<UserRole> userRoles) {
-		this.userRoles = userRoles;
-	}
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", username=" + username + ", password=" + password + ", firstName=" + firstName
-				+ ", lastName=" + lastName + ", email=" + email + ", phone=" + phone + ", enabled=" + enabled
-				+ ", profile=" + profile + ", userRoles=" + userRoles + "]";
-	}
-	
-	
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public Set<UserRole> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(Set<UserRole> userRoles) {
+        this.userRoles = userRoles;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", username=" + username + ", password=" + password + ", firstName=" + firstName
+                + ", lastName=" + lastName + ", email=" + email + ", phone=" + phone + ", enabled=" + enabled
+                + ", profile=" + profile + ", userRoles=" + userRoles + "]";
+    }
+
+
 }
