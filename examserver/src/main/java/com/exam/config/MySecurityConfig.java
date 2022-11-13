f020fb235912098b738321d1f90576b992b78f7f
aabhinaav <abhinavgargchand@gmail.com>
2022-11-13 22:59:07 +0530

    adding security configurations

---
+++ examserver/src/main/java/com/exam/config/MySecurityConfig.java
@@ -0,0 +1,56 @@
+package com.exam.config;
+
+import com.exam.services.impl.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity // it is to enable web security function in this class so as to use and modify them
+@Configuration // indicates it is configuration class containing configurations
+@EnableGlobalMethodSecurity(prePostEnabled = true) // to identify role and tell which will be accessed by user and which api is accessed by admin only
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(this.userDetailsServiceImpl).passwordEncoder(passwordEncoder());
+    }
+//    ye provide krti h info ki user exist krta h ya nhi using userDetailsServiceImpl class
+//    uske password ko encrypt krna hoga so we using BCryptPasswordEncoder
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .cors()
+                .disable()
+                .authorizeHttpRequests()
+                .antMatchers("/generate-token", "/user/createUser/").permitAll()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
+//WebSecurityConfigurerAdapter class has all security configuration methods(with default settings)
+//which we are going to modify in this file
