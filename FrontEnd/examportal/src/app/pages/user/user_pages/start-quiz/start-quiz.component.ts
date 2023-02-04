f7c452bdd1e39cea8b194e824167c85d7e4f0d48
aabhinaav <abhinavgargchand@gmail.com>
2023-02-04 13:24:58 +0530

    implementing functionality to print the result

--- FrontEnd/examportal/src/app/pages/user/user_pages/start-quiz/start-quiz.component.ts
+++ FrontEnd/examportal/src/app/pages/user/user_pages/start-quiz/start-quiz.component.ts
@@ -2,6 +2,7 @@ import { LocationStrategy } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { QuestionService } from 'src/services/question.service';
+import { LoginService } from 'src/services/login.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -25,7 +26,8 @@ export class StartQuizComponent implements OnInit {
     private locationStratergyObj: LocationStrategy,
     private activateRouteObj: ActivatedRoute,
     private questionServiceObj: QuestionService,
-    private routerObj: Router
+    private routerObj: Router,
+    private loginServiceObj: LoginService
   ) {}
 
   ngOnInit(): void {
@@ -132,32 +134,90 @@ export class StartQuizComponent implements OnInit {
 
   evluateQuiz_serverSide() {
     this.examSubmitted = true;
-    
-    this.questionServiceObj.evaluateQuizServerSide(this.questionsData).subscribe(
-      (res: any) => {
-        console.log(res);
 
-        this.marksGot = parseFloat(Number(res.marksGot).toFixed(2));
-        this.attempted = res.attempted;
-        this.correctAnswers = res.correctAnswers;
+    this.questionServiceObj
+      .evaluateQuizServerSide(this.questionsData)
+      .subscribe(
+        (res: any) => {
+          console.log(res);
 
-        console.log('attempted = ' + this.attempted);
-        console.log('correctAnswers = ' + this.correctAnswers);
-        console.log('marksGot = ' + this.marksGot);
-      },
-      (err) => {
-        console.log(err);
-        Swal.fire(
-          'Error !',
-          'Error in quiz evaluation on server side !!',
-          'error'
-        );
-      }
-    );
+          this.marksGot = parseFloat(Number(res.marksGot).toFixed(2));
+          this.attempted = res.attempted;
+          this.correctAnswers = res.correctAnswers;
+
+          console.log('attempted = ' + this.attempted);
+          console.log('correctAnswers = ' + this.correctAnswers);
+          console.log('marksGot = ' + this.marksGot);
+        },
+        (err) => {
+          console.log(err);
+          Swal.fire(
+            'Error !',
+            'Error in quiz evaluation on server side !!',
+            'error'
+          );
+        }
+      );
   }
 
   printTheResultFun() {
-    console.log('this function is used to print the result of quiz');
+    let username = this.loginServiceObj.getUserDetails()['username'];
+
+    let printContents = document.getElementById('resultSection')?.innerHTML;
+    let popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
+
+    if (popupWin) {
+      popupWin.document.open();
+      popupWin.document.write(`
+				<html>
+					<head>
+						<title>${username}</title>
+						<style type="text/css">
+              p {
+                font-family: "Times New Roman";
+              }
+              .padding-main-divcls{
+                padding: 5px;
+              }
+              .text-center{
+                text-align: center
+              }
+              .width-full{
+                width: 100%;
+              }
+              .box{
+                  border-style: solid;
+                  border-width: 1px;
+                  width: 65px;
+                  height: 100px;
+                  float: right;
+                  margin-right: 50px;
+                  font-size: 10px;
+                  padding: 5px;
+              }
+              .box-divcls{
+                width: 100%;
+                display: inline-block;
+              }
+              .TermsConditionTable, tr , td {
+								padding: 4px !important;
+							}
+							tr, td {
+								page-break-inside: avoid !important;
+							}
+							.break-after{
+								page-break-after: always;
+							}
+              .top-border-cls{
+                border-top: solid black 1.0pt;
+              }
+            </style>
+            <body onload="window.print();window.close()">${printContents}</body>
+          </head>
+        </html>
+      `);
+      popupWin.document.close();
+    }
   }
 
   startTimer() {
